0fce06cc074f622180e7b39700f69d18909f16ea
rysk1013 <rysk2908@gmail.com>
2021-05-13 03:26:17 +0900

    posts/edit,updateのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :users
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create, :edit, :update]
 end
