968b7a4ffb0df6968284f2cb5740aec698e8cfba
rysk1013 <rysk2908@gmail.com>
2021-05-15 04:06:54 +0900

    postsコントローラー単体テストの設定

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -6,17 +6,36 @@ RSpec.describe "PostsController", type: :request do
   end
 
   describe "GET #index" do
-    it "indexアクションにリクエストすると正常にレスポンスが帰ってくる" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
       get root_path
-      binding.pry
+      expect(response.status).to eq 200
     end
 
     it 'indexアクションにリクエストするとレスポンスに投稿済みのテキストが存在する' do
-      
+      get root_path
+      expect(response.body).to include(@post.text)
     end
 
     it 'indexアクションにリクエストするとレスポンスに投稿済みの画像が存在する' do
-      
+      get root_path
+      expect(response.body).to include("test_image.png")
+    end
+  end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get post_path(@post)
+      expect(response.status).to eq 200
+    end
+
+    it 'showアクションにリクエストをするとレスポンスに投稿済みのテキストが存在する' do
+      get root_path
+      expect(response.body).to include(@post.text)
+    end
+
+    it 'showアクションにリクエストをするとレスポンスに投稿済みの画像が存在する' do
+      get root_path
+      expect(response.body).to include("test_image.png")
     end
   end
 end
