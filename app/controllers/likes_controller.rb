36aa9f042843dd72a040eccb06535071630b11fd
rysk1013 <rysk2908@gmail.com>
2021-05-19 01:48:47 +0900

    likes#destroyアクションの設定

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,4 +5,10 @@ class LikesController < ApplicationController
     @like = Like.create(post_id: params[:post_id], user_id: current_user.id)
     redirect_back(fallback_location: root_path)
   end
+
+  def destroy
+    @already_liked = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @already_liked.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
