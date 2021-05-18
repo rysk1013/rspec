228988b5710a674d03d54e9e0842c0cfa5f8b5b2
rysk1013 <rysk2908@gmail.com>
2021-05-19 01:38:04 +0900

    likes#createアクションの作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,8 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    @like = Like.create(post_id: params[:post_id], user_id: current_user.id)
+    redirect_back(fallback_location: root_path)
+  end
 end
