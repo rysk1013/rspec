1fc07197c70479289d2c8b27ab755728b25fdb77
rysk1013 <rysk2908@gmail.com>
2021-05-19 01:39:04 +0900

    likes/destroyのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :users
   resources :posts do
-    resources :likes, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
 end
