dd652235ba408a675cc5601698f1e259b8e359ce
rysk1013 <rysk2908@gmail.com>
2021-05-26 17:31:49 +0900

    コメント削除の結合テスト設定

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -78,4 +78,53 @@ RSpec.describe "コメント", type: :system do
       end
     end
   end
+
+  describe 'コメント削除' do
+    before do
+      @post = FactoryBot.create(:post)
+      @comment = FactoryBot.create(:comment, post_id: @post.id, user_id: @post.user.id)
+    end
+
+    context 'コメント削除できるとき' do
+      it '投稿のuser_idとログインユーザーが一致すれば削除できる' do
+        #サインイン
+        sign_in(@post.user)
+        #投稿が存在するか確認
+        expect(page).to have_content(@post.text)
+        #showボタンをクリック
+        find('.post-show').find_link('show', href: post_path(@post)).click
+        #投稿詳細へ遷移しているか確認
+        expect(current_path).to eq post_path(@post)
+        #コメントが存在しているか確認
+        expect(page).to have_content(@comment.text)
+        #deleteボタンを押すとDBが-1減ることを確認
+        expect {
+          find('.delete-comment').find_link('delete', href: post_comment_path(@post.id, @comment.id)).click
+        }.to change{Comment.count}.by (-1)
+        #投稿詳細ページに遷移しているか確認
+        expect(current_path).to eq post_path(@post)
+        #コメントが存在していないか確認
+        expect(page).to have_no_content(@comment.text)
+      end
+    end
+
+    context 'コメント削除できないとき' do
+      it '投稿のuser_idとログインユーザーが一致しないと削除できない' do
+        #投稿を別のユーザーを作成
+        user = FactoryBot.create(:user)
+        #作成したユーザーでサインイン
+        sign_in(user)
+        #投稿が存在するか確認
+        expect(page).to have_content(@post.text)
+        #showボタンをクリック
+        find('.post-show').find_link('show', href: post_path(@post)).click
+        #投稿詳細ページへ遷移しているか確認
+        expect(current_path).to eq post_path(@post)
+        #コメント存在しているか確認
+        expect(page).to have_content(@comment.text)
+        #削除ボタンが存在していないことを確認
+        expect(page).to have_no_link('delete')
+      end
+    end
+  end
 end
